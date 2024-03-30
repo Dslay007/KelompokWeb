dae3116b8dd22e65145ba33d2a608cd470d04945
Your Name <you@example.com>
2024-03-31 00:40:20 +0700

    Pertemuan 7 - Praktikum 2

---
+++ empty.php
@@ -0,0 +1,20 @@
+<?php
+
+$nama = "";
+
+if (empty($nama)) {
+    echo "Nama tidak terdefinisi atau kosong";
+} else {
+    echo "Nama terdefinisi dan tidak kosong";
+}
+
+echo "<br><br>";
+
+if (empty($nonExistentVar)) {
+    echo "Nama tidak terdefinisi atau kosong";
+} else {
+    echo "Nama terdefinisi dan tidak kosong";
+}
+
+
+?>
