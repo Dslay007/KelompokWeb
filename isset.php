52112f90f6f8e9ec3250ab77feb0d9a6af209c65
Your Name <you@example.com>
2024-03-31 00:37:01 +0700

    Pertemuan 7 - Praktikum 1

--- isset.php
+++ isset.php
@@ -20,14 +20,7 @@
 
     echo "<br><br>";
 
-    $nama = "";
-
-    if (empty($nama)) {
-        echo "Nama tidak terdefinisi atau kosong";
-    } else {
-        echo "Nama terdefinisi dan tidak kosong";
-    }
-    
+   
     
 
 ?>
