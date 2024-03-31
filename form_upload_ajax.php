5101cc7bea99aafde9391b4e9dac1bb9a0d13a36
Your Name <you@example.com>
2024-04-01 00:17:16 +0700

    Pertemuan 8 -Praktikum 4

--- form_upload_ajax.php
+++ form_upload_ajax.php
@@ -2,18 +2,20 @@
 <html>
     <head>
         <title>Unggah File Dokumen</title>
-        
+        <link rel="stylesheet" href="upload.css" type="text/css">
     </head>
     <body>
-        <
+        <div class="upload-form-container">
             <h2>Unggah File Dokumen</h2>
             <form action="upload_ajax.php" id="upload-form" method="post" enctype="multipart/form-data">
-                
-                    <input type="file" name="file" id="file">
-                    <input type="submit" name="submit" value="unggah">
+                <div class="file-input-container">
+                    <input type="file" name="file" id="file" class="file-input">
+                    <label for="file" class="file-label">Pilih File</label>
+                </div>
+                <button type="submit" name="submit" class="upload-button" id="upload-button" disabled>Unggah</button>
             </form>
-
-            <div id="status"></div>
+            <div id="status" class="upload-status"></div>
+        </div>
         <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
         <script src="upload.js"></script>
     </body>
